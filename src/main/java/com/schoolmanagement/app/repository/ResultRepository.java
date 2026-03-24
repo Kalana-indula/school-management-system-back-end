38c65390c7cbf3298ed0982eda20281e856bf5c1
kalan <indulakalana@gmail.com>
2026-03-24 18:17:35 +0530

    Result repository added

---
+++ src/main/java/com/schoolmanagement/app/repository/ResultRepository.java
@@ -0,0 +1,9 @@
+package com.schoolmanagement.app.repository;
+
+import com.schoolmanagement.app.entity.Result;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ResultRepository extends JpaRepository<Result,Long> {
+}
