c78e8be465cdaf3508ff5b3445796e60000d799a
kalan <indulakalana@gmail.com>
2026-03-24 19:13:42 +0530

    EventRepository added

---
+++ src/main/java/com/schoolmanagement/app/repository/EventRepository.java
@@ -0,0 +1,9 @@
+package com.schoolmanagement.app.repository;
+
+import com.schoolmanagement.app.entity.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event,Long> {
+}
